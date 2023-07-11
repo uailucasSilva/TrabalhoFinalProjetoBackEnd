942fc2854daaac143b20ba5b19516702d923781a
uailucasSilva <lucas.humberto@estudante.iftm.edu.br>
2023-07-10 22:49:11 -0300

    Aumento do detalhamento da documentação Atendente

--- src/main/java/com/trabalhoFinal/trabalhoFinal/controllers/AtendenteController.java
+++ src/main/java/com/trabalhoFinal/trabalhoFinal/controllers/AtendenteController.java
@@ -4,6 +4,10 @@ import com.trabalhoFinal.trabalhoFinal.services.AtendenteService;
 import com.trabalhoFinal.trabalhoFinal.services.vo.AtendenteVO;
 import com.trabalhoFinal.trabalhoFinal.utils.MediaType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,7 +38,17 @@ public class AtendenteController {
   @GetMapping(produces = { MediaType.APPLICATION_JSON,
       MediaType.APPLICATION_XML,
       MediaType.APPLICATION_YML })
-  @Operation(tags = "Atendente", summary = "Busca Todos os atendentes")
+
+  @Operation(summary = "Retorna todos os atendentes.", description = "Retorna todos os atendentes.", tags = {
+      "Atendente" }, responses = {
+          @ApiResponse(description = "Sucesso", responseCode = "201", content = {
+              @Content(mediaType = MediaType.APPLICATION_JSON, array = @ArraySchema(schema = @Schema(implementation = AtendenteVO.class)))
+          }),
+          @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+          @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+          @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+          @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
+      })
   public ResponseEntity<List<AtendenteVO>> getAll() {
     List<AtendenteVO> all = service.getAll();
     return ResponseEntity.ok(all);
