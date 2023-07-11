839c5477f81ab3cf5846e696195d1ee7eebe06a8
uailucasSilva <lucas.humberto@estudante.iftm.edu.br>
2023-07-10 21:19:18 -0300

    Modificações no yml

--- src/main/java/com/trabalhoFinal/trabalhoFinal/TrabalhoFinalApplication.java
+++ src/main/java/com/trabalhoFinal/trabalhoFinal/TrabalhoFinalApplication.java
@@ -2,8 +2,10 @@ package com.trabalhoFinal.trabalhoFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class TrabalhoFinalApplication {
 
 	public static void main(String[] args) {
