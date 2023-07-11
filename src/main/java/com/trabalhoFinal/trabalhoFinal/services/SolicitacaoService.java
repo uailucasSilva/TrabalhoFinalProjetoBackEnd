2e30d2469ecd2dc47de8233ab5ded99aae98fdc7
natalia-arantes <natalia.arantes@estudante.iftm.edu.br>
2023-07-10 21:13:04 -0300

    Correção service de solicitações e mapper de dispositivo

--- src/main/java/com/trabalhoFinal/trabalhoFinal/services/SolicitacaoService.java
+++ src/main/java/com/trabalhoFinal/trabalhoFinal/services/SolicitacaoService.java
@@ -8,7 +8,6 @@ import com.trabalhoFinal.trabalhoFinal.services.mapper.vo.SolicitacaoVOMapper;
 import com.trabalhoFinal.trabalhoFinal.services.vo.SolicitacaoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
--- src/main/java/com/trabalhoFinal/trabalhoFinal/services/mapper/vo/DispositivoVOMapper.java
+++ src/main/java/com/trabalhoFinal/trabalhoFinal/services/mapper/vo/DispositivoVOMapper.java
@@ -1,4 +1,30 @@
 package com.trabalhoFinal.trabalhoFinal.services.mapper.vo;
 
-public class DispositivoVOMapper {
+import com.trabalhoFinal.trabalhoFinal.controllers.DispositivoController;
+import com.trabalhoFinal.trabalhoFinal.models.Dispositivo;
+import com.trabalhoFinal.trabalhoFinal.services.mapper.Mapper;
+import com.trabalhoFinal.trabalhoFinal.services.vo.DispositivoVO;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DispositivoVOMapper implements Mapper<Dispositivo, DispositivoVO> {
+
+    @Override
+    public DispositivoVO map(Dispositivo dispositivo) {
+
+        DispositivoVO dispositivoVO = DispositivoVO.builder()
+                .id(dispositivo.getId())
+                .nome(dispositivo.getNome())
+                .descricao(dispositivo.getDescricao())
+                .dataDeCadastro(dispositivo.getDataDeCadastro())
+                .dataDeDevolucao(dispositivo.getDataDeDevolucao())
+                .clienteId(dispositivo.getCliente().getId())
+                .build();
+
+        dispositivoVO.add(WebMvcLinkBuilder.linkTo(DispositivoController.class)
+                .slash(dispositivoVO.getId()).withSelfRel());
+
+        return dispositivoVO;
+    }
 }
